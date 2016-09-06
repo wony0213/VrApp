6fdb1565be65af6945f96c21775c5aeaa78d4e90
Wony <glf_develop@163.com>
2016-09-06 19:26:08 +0800

    增加SceneryListAdapter类、增加list_item_scenery.xml布局

---
+++ app/src/main/java/com/catr/test/vrapp/adapter/SceneryListAdapter.java
@@ -0,0 +1,30 @@
+package com.catr.test.vrapp.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by Wony on 2016/9/6.
+ */
+public class SceneryListAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
