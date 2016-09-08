f06f3e38d30f642f8b8abc9ca9c63eeb1290ad74
Wony <glf_develop@163.com>
2016-09-08 14:21:58 +0800

    1、修改按钮点击效果 2、修复Cardboard按钮功能

--- app/src/main/java/com/catr/test/vrapp/activity/LaunchActivity.java
+++ app/src/main/java/com/catr/test/vrapp/activity/LaunchActivity.java
@@ -152,7 +152,7 @@ public class LaunchActivity extends PermissionCheckActivity {
                         //monoBtn.setEnabled(false);
                         if (!isButtonClicked) {
                             isButtonClicked = true;
-                            startVrPanoActivity(VrWidgetView.DisplayMode.FULLSCREEN_STEREO);
+                            startVrPanoActivity(VrWidgetView.DisplayMode.FULLSCREEN_MONO);
                         }
 
                     }
